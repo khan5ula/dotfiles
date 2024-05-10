2927cb811ac294a309e69a72f24e2fd350f7a04c
Kristian Hannula <kristian.hannula@proton.me>
2024-05-10 10:11:42 +0300

    <C-e> neovim bind to close infuriating suggestions while in insert mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps herhttps://www.lazyvim.org/keymaps
+
+-- This Lua function will close all floating windows
+vim.keymap.set("i", "<C-e>", function()
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_config(win).relative ~= "" then
+      vim.api.nvim_win_close(win, false)
+    end
+  end
+end, { noremap = true, silent = true })
