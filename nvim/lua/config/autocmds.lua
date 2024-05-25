a317c7045434d1158c6ee8be2f75c629b8de8eca
Kristian Hannula <kristian.hannula@proton.me>
2024-05-25 13:00:28 +0300

    Disable single quote completion on Rust, minor configs

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -11,3 +11,31 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.wrap = true
   end,
 })
+
+-- Disable single quote pairing in Rust
+local rust_group = vim.api.nvim_create_augroup("Rust_disable_single_quote", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  group = rust_group,
+  callback = function()
+    if MiniPairs ~= nil then
+      MiniPairs.unmap("i", "'", "''")
+    end
+  end,
+  desc = "Disable single quote Rust",
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyLoad",
+  group = vim.api.nvim_create_augroup("Rust_disable_single_quote_lazyload", { clear = true }),
+  callback = function(event)
+    if event.data == "mini.pairs" then
+      vim.api.nvim_exec_autocmds("FileType", {
+        pattern = "rust",
+        group = rust_group,
+      })
+    end
+  end,
+  desc = "Disable single quote Rust",
+})
