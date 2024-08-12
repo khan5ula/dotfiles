14610e7a00306340b73840e90e80e65fa2a0b417
Kristian Hannula <kristian.hannula@proton.me>
2024-08-12 19:58:46 +0300

    timely update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,4 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.wrap = true
+opt.wrap = false
+--vim.g.root_spec = { "cwd" }
